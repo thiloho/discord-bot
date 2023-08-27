2b4309cef6b7fbfcabe00c06e8dc15f343988428
thiloho <123883702+thiloho@users.noreply.github.com>
2023-08-27 19:03:40 +0200

    Add simple level system

--- commands/fun/level.js
+++ commands/fun/level.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { Users } = require('../../dbObjects.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -15,10 +16,19 @@ module.exports = {
 				.setDescription('Display rewards for certain level milestones')),
 	async execute(interaction) {
 		if (interaction.options.getSubcommand() === 'info') {
-			// test
+			const target = interaction.options.getUser('target') || interaction.user;
+			const user = await Users.findOne({ where: { user_id: target.id } });
+			if (user) {
+				const neededXP = 10 * Math.pow(2, user.level - 1);
+				await interaction.reply(`Level: ${user.level}\nXP: ${user.experience_points}/${neededXP}`);
+			}
+			else {
+				await interaction.reply('This user has not started leveling up yet.');
+			}
 		}
 		else if (interaction.options.getSubcommand() === 'rewards') {
-			// test
+			const rewards = 'Level 5: Reward 1\nLevel 10: Reward 2\nLevel 15: Reward 3';
+			await interaction.reply(`Level rewards:\n${rewards}`);
 		}
 	},
 };
--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,10 +1,45 @@
 const { Events } = require('discord.js');
-const { addBalance } = require('../shared-currency-functions');
+const { Users } = require('../dbObjects.js');
 
 module.exports = {
 	name: Events.MessageCreate,
-	execute(message) {
+	async execute(message) {
 		if (message.author.bot) return;
-		addBalance(message.author.id, 1);
+
+		const { author } = message;
+		const now = Date.now();
+
+		const { cooldowns } = message.client;
+
+		if (cooldowns.has(author.id) && cooldowns.get(author.id) > now) {
+			return;
+		}
+
+		cooldowns.set(author.id, now + 3000);
+
+		const randomXP = Math.random() < 0.05 ? 2 : 1;
+
+		const user = await Users.findOne({ where: { user_id: author.id } });
+		if (user) {
+			user.experience_points += randomXP;
+			const neededXP = 10 * Math.pow(2, user.level - 1);
+			if (user.experience_points >= neededXP) {
+				user.level += 1;
+				user.experience_points = 0;
+				message.reply(`Congratulations! You have leveled up to level ${user.level}!`);
+
+				if (user.level === 3) {
+					const roleId = '1145397499784335501';
+					const { member } = message;
+
+					member.roles.add(roleId);
+					message.reply('You were given the `Active` role because you reached level 3. Congratulations!');
+				}
+			}
+			await user.save();
+		}
+		else {
+			await Users.create({ user_id: author.id, experience_points: randomXP });
+		}
 	},
 };
