0d4ed248f21f8be13929b0ed8801edb6132258cc
thiloho <123883702+thiloho@users.noreply.github.com>
2023-09-23 18:30:43 +0200

    Improve flake remove new level command

--- flake.nix
+++ flake.nix
@@ -1,47 +1,28 @@
 {
-  description = "JavaScript development environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-  };
-
-  outputs = { self, nixpkgs }:
-    let
-      # Systems supported
-      allSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        nodejs_20
+        tree
       ];
-      
-      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
-        pkgs = nixpkgs.legacyPackages.${system};
-      });
-    in
-    {
-      devShells = forAllSystems ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            nodejs_20
-            tree
-          ];
-          shellHook = ''
-            alias reinit='node dbInit.js --force && node deploy-commands.js && node index.js'
-          '';
-        };
-      });
-      packages = forAllSystems ({ pkgs }: {
-        default = pkgs.buildNpmPackage {
-          name = "build-denbot";
-          src = ./.;
-          npmDepsHash = "sha256-NRoyh++f5/NotnT6w59OtSYW2pKwJ04H32Ujy/adCNE=";
-          dontNpmBuild = true;
-          installPhase = ''
-            mkdir $out
-            cp -r * $out
-          '';
-        };
-      });
+      shellHook = ''
+        alias reinit='node dbInit.js --force && node deploy-commands.js && node index.js'
+      '';
     };
+    packages.${system}.default = pkgs.buildNpmPackage {
+      name = "build-denbot";
+      src = ./.;
+      npmDepsHash = "sha256-NRoyh++f5/NotnT6w59OtSYW2pKwJ04H32Ujy/adCNE=";
+      dontNpmBuild = true;
+      installPhase = ''
+        mkdir $out
+        cp -r * $out
+      '';
+    };
+  };
 }
